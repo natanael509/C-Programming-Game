0ee0c64b722f66843494a38be74da2c047551e33
NATANAEL JOSE TRIBESS <natanael.tribess@fiesc.com.br>
2026-08-13 22:20:27 -0300

    Segundo commit adicionando bibliotecas e funções

--- Projeto.cpp
+++ Projeto.cpp
@@ -1,15 +1,47 @@
+//bibliotecas
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <conio.h>
+
+
 typedef struct {
 	int x;
 	int y;
 
-} posicao;
+} posicao;//estrutura da posiçao
 
 typedef struct {
-	char nome[50];
+	char nome[25];
 	int vida;
 	int vidaMax;
 	int ataque;
 	int nivel;
 	posicao pos;
 	int chaves;
-} Jogador;
+} Jogador;//estrutura do que compoe o jogador
+
+
+void iniciarJogador(Jogador *j, const char *nomeInicial){
+	strcpy(j->nome, nomeInicial);
+//Status iniciais do nosso jogador
+	j->vidaMax = 100;
+	j->vida = 100;
+	j->nivel = 1;
+	j->ataque = 10;
+	j->chaves = 0;
+//Posição inicial
+	j->pos.x = 1;
+	j->pos.y = 1;
+}
+int main(){
+	Jogador player;
+	
+	iniciarJogador (&player, "Heroi");
+	printf("Jogador %c criado com sucesso!\n", player.nome);
+	printf("Vida: %d de %d\n", player.vida, player.vidaMax);
+	printf("Posicao inicial do jogador: %d, %d\n", player.pos.x, player.pos.y);
+	
+	return 0;
+}
