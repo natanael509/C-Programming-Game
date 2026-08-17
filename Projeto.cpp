acada15c23fef8f74d0fbf7b24698d1be3829b13
NATANAEL JOSE TRIBESS <natanael.tribess@fiesc.com.br>
2026-08-16 23:09:58 -0300

    Quinto commit adicionando um mapa

--- Projeto.cpp
+++ Projeto.cpp
@@ -12,7 +12,22 @@
 #define SETA_DIREITA 77
 #define TECLA_ESC 27
 
+#define LARGURA 10
+#define ALTURA 10
 
+// Mapa global inicial do jogo
+char mapa[ALTURA][LARGURA] = {
+    {'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'},
+    {'X', '.', '.', '.', 'X', '.', '.', '.', '.', 'X'},
+    {'X', '.', '^', '.', 'X', '.', 'E', '.', '.', 'X'}, // '^' é espinho
+    {'X', '.', '.', '.', '.', '.', '.', '.', '.', 'X'}, //'E' é inimigo
+    {'X', 'X', 'X', '.', 'X', 'X', 'X', 'X', '.', 'X'},
+    {'X', '.', '.', '.', '.', '.', '^', '.', '.', 'X'},
+    {'X', '.', 'E', '.', 'X', '.', '.', '.', '.', 'X'},
+    {'X', '.', '.', '.', 'X', '.', '.', '.', '.', 'X'},
+    {'X', '.', '.', '.', 'X', '.', '.', '.', '.', 'X'},
+    {'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'}
+};
 
 typedef struct {
 	int x;
@@ -48,33 +63,60 @@ void moverJogador(Jogador *j, int tecla){
 	if (tecla == 224 || tecla == 0){
 		tecla = _getch();
 	}
+	
+	int direcaoX = j->pos.x;
+	int direcaoY j->pos.y;
+	
 	switch(tecla){
 	//cima
 	case 'w': case 'W':
     case SETA_CIMA:
-    j->pos.y--;
+    direcaoY--;
 	break;
 	
 	//baixo
 	case 's': case 'S':
     case SETA_BAIXO:
-    j->pos.y++;
-	break;
-	
-	//direito
-	case 'a': case 'A':
-    case SETA_ESQUERDA:
-    j->pos.x--;
+    direcaoY++;
 	break;
 	
 	//esquerda
-	case 'd': case 'D':
-    case SETA_DIREITA:
-    j->pos.x++;
+	case 'a': case 'A':
+    case SETA_ESQUERDA:
+  	direcaoX--;
 	break;
 	
-	default:
-    return;
+	//direita
+	case 'd': case 'D':
+    case SETA_DIREITA:
+    direcaoX++;
+	break;
+	
+	default: return;
+    
+	}
+	
+	//fazer a checagem de colisão(alterando apenas a copia da função)
+	if (mapa[direcaoY][direcaoX] == 'X'){
+		printf("/nVocê colidiu com a parede!/n")
+		return; //quando retorna aqui faz o cancelamento do movimento
+	}
+	if (mapa[direcaoY][direcaoX]=='^'){
+		int dano = 20;
+		j->vida -= dano;
+		printf("\nVocê caiu na armadilha e levou %d de dano/n", dano);
+	}
+	if (mapa[direcaoY][direcaoX]== 'E'){
+		int dano = 35;
+		j->vida -= dano;
+		printf("/nVoce enfrentou um inimigo! Perdeu %d de vida!\n", dano);
+		mapa[novoY][novoX] = '.';
+	}
+	
+	if(j->vida>0){
+		j->pos.x = direcaoX;
+		j->pos.y = direcaoY;
+		}	
 	}
 	printf("Jogador se moveu para (%d, %d)\n", j->pos.x, j->pos.y);
 }
@@ -84,7 +126,7 @@ int main(){
 	Jogador player;
 	
 	iniciarJogador (&player, "Heroi");
-	printf("Jogador %c criado com sucesso!\n", player.nome);
+	printf("Jogador %s criado com sucesso!\n", player.nome);
 	printf("Vida: %d de %d\n", player.vida, player.vidaMax);
 	printf("Posicao inicial do jogador: %d, %d\n", player.pos.x, player.pos.y);
 	
